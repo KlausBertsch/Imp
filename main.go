0560152cecbc087f1df153e328c43371c163d0ee
KlausBertsch <80857787+KlausBertsch@users.noreply.github.com>
2022-06-20 17:48:22 +0200

    Local Variables can now be used!

--- main.go
+++ main.go
@@ -44,8 +44,14 @@ type Val struct {
     flag Kind
     valI int
     valB bool
-    local bool
 }
+
+//key struct to identify Vals in a hierarchical manner and to allow local variables with same names
+type key struct { //as higher level ones
+    name string
+    level int
+}
+
 
 func mkInt(x int) Val {
     return Val{flag: ValueInt, valI: x}
@@ -94,23 +100,23 @@ func showType(t Type) string {
 }
 
 // Value State is a mapping from variable names to values
-type ValState map[string]Val
 
+type ValState map[key] Val
 // Value State is a mapping from variable names to types
-type TyState map[string]Type
+type TyState map[key]Type
 
 // Interface
 
 type Exp interface {
     pretty() string
-    eval(s ValState) Val
-    infer(t TyState) Type
+    eval(s ValState,level int) Val
+    infer(t TyState,level int) Type
 }
 
 type Stmt interface {
     pretty() string
-    eval(s ValState)
-    check(t TyState) bool
+    eval(s ValState,level int)
+    check(t TyState,level int) bool
 }
 
 // Statement cases (incomplete)
@@ -153,7 +159,6 @@ type Or [2]Exp
 type Neg [1]Exp
 type Equ [2]Exp
 type Less [2]Exp
-type Group [1]Exp
 type Var string
 
 /////////////////////////
@@ -168,7 +173,7 @@ func (stmt Seq) pretty() string {
 func (decl Decl) pretty() string {
     return decl.lhs + " := " + decl.rhs.pretty()
 }
-
+//spaces added for better appearance of print
 func (ite IfThenElse) pretty() string {
     return "if " + ite.cond.pretty() + " { " + ite.thenStmt.pretty() + " } else { " + ite.elseStmt.pretty() +" }"
 }
@@ -181,29 +186,31 @@ func (assign Assign) pretty() string {
     return assign.lhs + " = " + assign.rhs.pretty()
 }
 
+//spaces added for better appearance of print
 func (while While) pretty() string {
     return "while " + while.cond.pretty() + " { " + while.whileStmt.pretty() + " }"
 }
 
 // eval
 
-func (stmt Seq) eval(s ValState) {
-    stmt[0].eval(s)
-    stmt[1].eval(s)
+func (stmt Seq) eval(s ValState,level int) {
+    stmt[0].eval(s,level)//evaluate both statements
+    stmt[1].eval(s,level)
 }
 
-func (ite IfThenElse) eval(s ValState) {
-    v := ite.cond.eval(s)
+func (ite IfThenElse) eval(s ValState,level int) {
+    v := ite.cond.eval(s,level)
     if v.flag == ValueBool {
-        s2 := make(map[string]Val)
-        for k,val := range s {
-            s2[k] = val
-        }
         switch {
         case v.valB:
-            ite.thenStmt.eval(s2)
+            ite.thenStmt.eval(s,level+1)//add a level to the  Val map, so local variables can be added
         case !v.valB:
-            ite.elseStmt.eval(s2)
+            ite.elseStmt.eval(s,level+1)//add a level to the  Val map, so local variables can be added
+        }
+        for k, _ := range s {// delete all variables added in the If-Then-Else Statement
+            if k.level >= level+1 {
+                delete(s,k)
+            }
         }
 
     } else {
@@ -212,109 +219,115 @@ func (ite IfThenElse) eval(s ValState) {
 
 }
 
-func (while While) eval(s ValState) {
-    v := while.cond.eval(s)
+func (while While) eval(s ValState,level int) {
+    v := while.cond.eval(s,level)
     if v.flag == ValueBool {
-        s2 := make(map[string]Val)
-        for k,val := range s {
-            s2[k] = val
-        }
         for v.valB {
-            while.whileStmt.eval(s2)
-            if fmt.Sprintf("%T", while.whileStmt) == "Decl" {
-                _,ok := while.whileStmt.(Decl)
-                fmt.Printf("%b",ok)
-            }
-            v = while.cond.eval(s2)
+            while.whileStmt.eval(s,level+1) //add a level to the  Val map, so local variables can be added
+            v = while.cond.eval(s,level) //evaluation of the condition can only happen on the initial one
             if v.flag != ValueBool {
                 fmt.Printf("while eval fail")
             }
         }
-        for k,val := range s2 {
-            if _, ok := s[k]; ok {
-                if !val.local {
-                    s[k] = s2[k]
-                }
+        for k, _ := range s { // delete all variables added in the While loop
+            if k.level >= level+1 {
+                delete(s,k)
             }
         }
-
     } else {
         fmt.Printf("while eval fail")
     }
 }
 
-func (printer Print) eval(s ValState) {
-    x := printer.toPrint.eval(s)
-    fmt.Printf("\n printer: %s", showVal(x))
+func (printer Print) eval(s ValState,level int) {
+    x := printer.toPrint.eval(s,level)
+    fmt.Printf("\n printer: %s", showVal(x))//Expression is also printed, after evaluation
 }
 
 // Maps are represented via points.
 // Hence, maps are passed by "reference" and the update is visible for the caller as well.
-func (decl Decl) eval(s ValState) {
-    v := decl.rhs.eval(s)
+func (decl Decl) eval(s ValState,level int) {
+    v := decl.rhs.eval(s,level)
     x := (string)(decl.lhs)
-    s[x] = v
+    s[key{x,level}] = v //add new variable on given level to ValState
 }
 
-func (assign Assign) eval(s ValState) {
-    v := assign.rhs.eval(s)
+func (assign Assign) eval(s ValState,level int) {
+    v := assign.rhs.eval(s,level)
     x := (string)(assign.lhs)
-    if _, ok := s[x]; ok {
-        s[x] = v
+    for i := level; i >= 0; i-- {//check for the variable on all higher levels, if there are any 
+    k := key{x,i}
+    if _, ok := s[k]; ok {//assign variable on highest level, a variable with that name exists
+        s[k] = v
+    }
     }
 }
 
 // type check
 
-func (stmt Seq) check(t TyState) bool {
-    if !stmt[0].check(t) {
+func (stmt Seq) check(t TyState,level int) bool {
+    if !stmt[0].check(t,level) {//return false, since first statement is alrady not correctly typed
         return false
     }
-    return stmt[1].check(t)
+    return stmt[1].check(t,level)//if first one was correctly typed, return the evaluation for the second statement
 }
 
-func (decl Decl) check(t TyState) bool {
-    ty := decl.rhs.infer(t)
-    if ty == TyIllTyped {
+func (decl Decl) check(t TyState,level int) bool {
+    ty := decl.rhs.infer(t,level)
+    if ty == TyIllTyped { //Expression not correctly typed
         return false
     }
-
     x := (string)(decl.lhs)
-    t[x] = ty
+    t[key{x,level}] = ty //set type on given level
     return true
 }
 
-func (a Assign) check(t TyState) bool {
+func (a Assign) check(t TyState,level int) bool {
         x := (string)(a.lhs)
-        if val, ok := t[x]; ok {
-            return val == a.rhs.infer(t)
-        } else {
-            return false
+        for i := level; i >= 0; i-- {//check for the variable on all higher levels, if there are any 
+            if val, ok := t[key{x,i}]; ok { //check whether variable exists
+                return val == a.rhs.infer(t,level)  //if variable exists return whether given expression is
+            }                                       //of correct type for the variable
         }
+        return false // no variable with given name was found, return false
         
 }
 
-func (ite IfThenElse) check(t TyState) bool {
-    ty := ite.cond.infer(t)
+func (ite IfThenElse) check(t TyState,level int) bool {
+    ret := false
+    ty := ite.cond.infer(t,level)
     if ty == TyIllTyped {
-        return false
+        return false //Expression not correctly typed, statements do not have to be checked
     }
-    if !ite.thenStmt.check(t) {
-        return false
+    if !ite.thenStmt.check(t,level+1) {//add a level for local variables in If-Then-Else Statement
+        ret = false
     }
-    return ite.elseStmt.check(t)
+    ret = ite.elseStmt.check(t,level+1)//add a level for local variables in If-Then-Else Statement
+    for k, _ := range t { // delete all types of variables added in the If-Then-Else Statement
+        if k.level >= level+1 {
+            delete(t,k)
+        }
+    }
+    return ret
 }
 
-func (while While) check(t TyState) bool {
-    ty := while.cond.infer(t)
+func (while While) check(t TyState,level int) bool {
+    ret := false
+    ty := while.cond.infer(t,level)
     if ty == TyIllTyped {
-        return false
+        return false//Expression not correctly typed, statements do not have to be checked
     }
-    return while.whileStmt.check(t)
+    ret = while.whileStmt.check(t,level) //add a level for local variables in While Loop
+    for k, _ := range t { // delete all types of variables added in the While loop
+        if k.level >= level+1 {
+            delete(t,k)
+        }
+    }
+    return ret
 }
 
-func (printer Print) check(t TyState) bool {
-    ty := printer.toPrint.infer(t)
+func (printer Print) check(t TyState,level int) bool {
+    ty := printer.toPrint.infer(t,level)
     return ty != TyIllTyped
 }
 
@@ -420,214 +433,189 @@ func (e Less) pretty() string {
     return x
 }
 
-func (e Group) pretty() string {
-        var x string
-    x = "("
-    x += e[0].pretty()
-    x += ")"
-
-    return x
-}
-
 // Evaluator
 
-func (x Bool) eval(s ValState) Val {
+func (x Bool) eval(s ValState,level int) Val {
     return mkBool((bool)(x))
 }
 
-func (x Num) eval(s ValState) Val {
+func (x Num) eval(s ValState,level int) Val {
     return mkInt((int)(x))
 }
 
-func (e Mult) eval(s ValState) Val {
-    n1 := e[0].eval(s)
-    n2 := e[1].eval(s)
+func (e Mult) eval(s ValState,level int) Val {
+    n1 := e[0].eval(s,level)
+    n2 := e[1].eval(s,level)
     if n1.flag == ValueInt && n2.flag == ValueInt {
         return mkInt(n1.valI * n2.valI)
     }
-    return mkUndefined()
+    return mkUndefined()//wrong type was given in either Expression
 }
 
-func (e Plus) eval(s ValState) Val {
-    n1 := e[0].eval(s)
-    n2 := e[1].eval(s)
+func (e Plus) eval(s ValState,level int) Val {
+    n1 := e[0].eval(s,level)
+    n2 := e[1].eval(s,level)
     if n1.flag == ValueInt && n2.flag == ValueInt {
         return mkInt(n1.valI + n2.valI)
     }
-    return mkUndefined()
+    return mkUndefined()//wrong type was given in either Expression
 }
 
-func (e And) eval(s ValState) Val {
-    b1 := e[0].eval(s)
-    b2 := e[1].eval(s)
+func (e And) eval(s ValState,level int) Val {
+    b1 := e[0].eval(s,level)
+    b2 := e[1].eval(s,level)
     switch {
     case b1.flag == ValueBool && b1.valB == false:
-        return mkBool(false)
+        return mkBool(false) //first Expression is boolean and false, no more evaluation necessary
     case b1.flag == ValueBool && b2.flag == ValueBool:
-        return mkBool(b1.valB && b2.valB)
+        return mkBool(b1.valB && b2.valB) //both Expressions are boolean, return Evaluation
     }
-    return mkUndefined()
+    return mkUndefined() //wrong type was given in either Expression
 }
 
-func (e Or) eval(s ValState) Val {
-    b1 := e[0].eval(s)
-    b2 := e[1].eval(s)
+func (e Or) eval(s ValState,level int) Val {
+    b1 := e[0].eval(s,level)
+    b2 := e[1].eval(s,level)
     switch {
     case b1.flag == ValueBool && b1.valB == true:
-        return mkBool(true)
+        return mkBool(true) //first Expression is boolean and true, no more evaluation necessary
     case b1.flag == ValueBool && b2.flag == ValueBool:
-        return mkBool(b1.valB || b2.valB)
+        return mkBool(b1.valB || b2.valB) //both Expressions are boolean, return Evaluation
     }
-    return mkUndefined()
+    return mkUndefined()//wrong type was given in either Expression
 }
 
-func (e Neg) eval(s ValState) Val {
-    b := e[0].eval(s)
-    if(b.flag == ValueBool){
+func (e Neg) eval(s ValState,level int) Val {
+    b := e[0].eval(s,level)
+    if(b.flag == ValueBool){ //only negate if val is of type boolean
         return mkBool(!b.valB)
     }
-    return mkUndefined()
+    return mkUndefined()//wrong type was given in Expression
 }
 
-func (e Equ) eval(s ValState) Val {
-    b1 := e[0].eval(s)
-    b2 := e[1].eval(s)
-    switch {
+func (e Equ) eval(s ValState,level int) Val {
+    b1 := e[0].eval(s,level)
+    b2 := e[1].eval(s,level)
+    switch { //can only check for equality of Vals with same type
     case b1.flag == ValueBool && b1.flag == ValueBool:
         return mkBool(b1.valB == b2.valB)
     case b1.flag == ValueInt && b2.flag == ValueInt:
         return mkBool(b1.valI == b2.valI)
     }
-    return mkUndefined()
+    return mkUndefined() //wrong type was given in either Expression or they are not of the same type
 }
 
-func (e Less) eval(s ValState) Val {
-    b1 := e[0].eval(s)
-    b2 := e[1].eval(s)
-    if(b1.flag == ValueInt && b2.flag == ValueInt){
+func (e Less) eval(s ValState,level int) Val {
+    b1 := e[0].eval(s,level)
+    b2 := e[1].eval(s,level)
+    if(b1.flag == ValueInt && b2.flag == ValueInt){//only ints can be compared with lesser
         return mkBool(b1.valI < b2.valI)
     }
-    return mkUndefined()
+    return mkUndefined()//wrong type was given in either Expression
 }
 
-func (e Group) eval(s ValState) Val {
-    b := e[0].eval(s)
-    switch{
-    case b.flag == ValueBool:
-        return mkBool(b.valB)
-    case b.flag == ValueInt:
-        return mkInt(b.valI)
-    }
-    return mkUndefined()
-}
-
-func(e Var) eval (s ValState) Val {
+func(e Var) eval (s ValState,level int) Val {
     y := (string)(e)
-    b := s[y]
-    switch{
-    case b.flag == ValueBool:
-        return mkBool(b.valB)
-    case b.flag == ValueInt:
-        return mkInt(b.valI)
+    for i := level; i >= 0; i-- { //search for var in highest level
+        k := key{y,i}
+        if b, ok := s[k]; ok {//var exists check for type and return value
+        switch{
+        case b.flag == ValueBool:
+            return mkBool(b.valB)
+        case b.flag == ValueInt:
+            return mkInt(b.valI)
+        }
+        return mkUndefined() //Variable is incorrectly typed
+        }
     }
-    return mkUndefined()
+    return mkUndefined() //Variable does not exist
 }
 // Type inferencer/checker
 
-func (x Var) infer(t TyState) Type {
+func (x Var) infer(t TyState,level int) Type {
     y := (string)(x)
-    ty, ok := t[y]
-    if ok {
-        return ty
-    } else {
-        return TyIllTyped // variable does not exist yields illtyped
+    for i := level; i >= 0; i-- { //iterate through Val levels from top to bottom
+        k := key{y,i}
+        if ty, ok := t[k]; ok { //give back type of variable on highest level
+            return ty
+        }
     }
-
+    return TyIllTyped // variable does not exist yields illtyped
 }
 
-func (x Bool) infer(t TyState) Type {
+func (x Bool) infer(t TyState,level int) Type {
     return TyBool
 }
 
-func (x Num) infer(t TyState) Type {
+func (x Num) infer(t TyState,level int) Type {
     return TyInt
 }
 
-func (e Mult) infer(t TyState) Type {
-    t1 := e[0].infer(t)
-    t2 := e[1].infer(t)
+func (e Mult) infer(t TyState,level int) Type {
+    t1 := e[0].infer(t,level)
+    t2 := e[1].infer(t,level)
     if t1 == TyInt && t2 == TyInt {
         return TyInt
     }
-    return TyIllTyped
+    return TyIllTyped //wrong type was given in either Expression
 }
 
-func (e Plus) infer(t TyState) Type {
-    t1 := e[0].infer(t)
-    t2 := e[1].infer(t)
+func (e Plus) infer(t TyState,level int) Type {
+    t1 := e[0].infer(t,level)
+    t2 := e[1].infer(t,level)
     if t1 == TyInt && t2 == TyInt {
         return TyInt
     }
-    return TyIllTyped
+    return TyIllTyped //wrong type was given in either Expression
 }
 
-func (e And) infer(t TyState) Type {
-    t1 := e[0].infer(t)
-    t2 := e[1].infer(t)
+func (e And) infer(t TyState,level int) Type {
+    t1 := e[0].infer(t,level)
+    t2 := e[1].infer(t,level)
     if t1 == TyBool && t2 == TyBool {
         return TyBool
     }
-    return TyIllTyped
+    return TyIllTyped //wrong type was given in either Expression
 }
 
-func (e Or) infer(t TyState) Type {
-    t1 := e[0].infer(t)
-    t2 := e[1].infer(t)
+func (e Or) infer(t TyState,level int) Type {
+    t1 := e[0].infer(t,level)
+    t2 := e[1].infer(t,level)
     if t1 == TyBool && t2 == TyBool {
         return TyBool
     }
-    return TyIllTyped
+    return TyIllTyped //wrong type was given in either Expression
 }
 
-func (e Neg) infer(t TyState) Type {
-    t1 := e[0].infer(t)
+func (e Neg) infer(t TyState,level int) Type {
+    t1 := e[0].infer(t,level)
     if t1 == TyBool {
         return TyBool
     }
-    return TyIllTyped
+    return TyIllTyped //wrong type was given in Expression
 }
 
-func (e Equ) infer(t TyState) Type {
-    t1 := e[0].infer(t)
-    t2 := e[1].infer(t)
-    if t1 == TyBool && t2 == TyBool {
+func (e Equ) infer(t TyState,level int) Type {
+    t1 := e[0].infer(t,level)
+    t2 := e[1].infer(t,level)
+    if t1 == TyBool && t2 == TyBool {//can only check for equality of Vals with same type
         return TyBool
     }
-    if t1 == TyInt && t2 == TyInt {
+    if t1 == TyInt && t2 == TyInt {//can only check for equality of Vals with same type
         return TyBool
     }
-    return TyIllTyped
+    return TyIllTyped //wrong type was given in either Expression or Expressions are not of same type
 }
 
-func (e Less) infer(t TyState) Type {
-    t1 := e[0].infer(t)
-    t2 := e[1].infer(t)
-    if t1 == TyInt && t2 == TyInt {
+func (e Less) infer(t TyState,level int) Type {
+    t1 := e[0].infer(t,level)
+    t2 := e[1].infer(t,level )
+    if t1 == TyInt && t2 == TyInt {//only ints can be compared with lesser
         return TyInt
     }
-    return TyIllTyped
+    return TyIllTyped //wrong type was given in either Expression
 }
 
-func (e Group) infer(t TyState) Type {
-    t1 := e[0].infer(t)
-    if t1 == TyInt {
-        return TyInt
-    }
-    if t1 == TyBool {
-        return TyBool
-    }
-    return TyIllTyped
-}
 // Helper functions to build ASTs by hand
 
 func number(x int) Exp {
@@ -672,14 +660,11 @@ func less(x, y Exp) Exp {
     return (Less)([2]Exp{x, y})
 }
 
-func group(x Exp) Exp {
-    return (Group)([1]Exp{x})
-}
-
 func variable(x string) Exp {
     return (Var)(x)
 }
 
+//Since Statements are structs, Initialization needs more code
 func ite (x Exp, y,z Stmt) Stmt {
     var ite IfThenElse
     ite.cond = x
@@ -725,26 +710,25 @@ func seq (x,y Stmt) Stmt {
 // Examples
 
 func run(e Exp) {
-    s := make(map[string]Val)
-    t := make(map[string]Type)
+    s := make(map[key]Val)
+    t := make(map[key]Type)
     fmt.Printf("\n ******* ")
     fmt.Printf("\n %s", e.pretty())
-    fmt.Printf("\n %s", showVal(e.eval(s)))
-    fmt.Printf("\n %s", showType(e.infer(t)))
+    fmt.Printf("\n %s", showVal(e.eval(s,0)))
+    fmt.Printf("\n %s", showType(e.infer(t,0)))
 }
-
+//Added, to run Statements
 func runSt(st Stmt) {
-    s := make(map[string]Val)
-    t := make(map[string]Type)
+    s := make(map[key]Val)
+    t := make(map[key]Type)
     fmt.Printf("\n ******* ")
     fmt.Printf("\n %s", st.pretty())
-    st.eval(s)
-    fmt.Printf("\n check: %t", st.check(t))
+    st.eval(s,0) //Statement Evaluation does not give a return, so it is only executed
+    fmt.Printf("\n check: %t", st.check(t,0))
 }
 
 func ex1() {
     ast := plus(mult(number(1), number(2)), number(0))
-
     run(ast)
 }
 
@@ -782,13 +766,18 @@ func st5() {
     ast := seq(decl("x",boolean(true)),seq(ite(variable("x"),assign("x",boolean(false)),assign("x",boolean(true))),printer(variable("x"))))
     runSt(ast)
 }
-func st6() {
+func st6() { //test for assignment of variables in higher order
     ast := seq(decl("x",boolean(false)),seq(ite(variable("x"),assign("x",boolean(false)),assign("x",boolean(true))),printer(variable("x"))))
     runSt(ast)
 }
 
-func st7() {
+func st7() { //test for usage of local variables with same names as higher order ones
     ast := seq(decl("x",number(1)),seq(ite(less(variable("x"),number(1)),decl("x",number(2)),decl("x",number(3))),printer(variable("x"))))
+    runSt(ast)
+}
+
+func st8() { //test for usage of local variables with same names as higher order ones
+    ast := seq(decl("x",boolean(true)),seq(while(variable("x"),seq(assign("x",boolean(false)),ite(boolean(true),decl("x",number(1)),decl("x",number(2))))),printer(variable("x"))))
     runSt(ast)
 }
 
@@ -805,4 +794,5 @@ func main() {
     st4()
     st5()
     st7()
-}
+    st8()
+} 
